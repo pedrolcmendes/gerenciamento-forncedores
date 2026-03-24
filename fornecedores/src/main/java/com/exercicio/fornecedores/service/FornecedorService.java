cb0cf6786c26c773ac453586aefc1c6460fabd43
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-24 15:29:23 -0300

    add service Fornecedor CRUD

---
+++ fornecedores/src/main/java/com/exercicio/fornecedores/service/FornecedorService.java
@@ -0,0 +1,34 @@
+package com.exercicio.fornecedores.service;
+
+import com.exercicio.fornecedores.entity.Fornecedor;
+import com.exercicio.fornecedores.repository.FornecedorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FornecedorService {
+
+    private final FornecedorRepository fornecedorRepository;
+
+    public FornecedorService(FornecedorRepository fornecedorRepository) {
+        this.fornecedorRepository = fornecedorRepository;
+    }
+
+    public Fornecedor salvar(Fornecedor fornecedor) {
+        return fornecedorRepository.save(fornecedor);
+    }
+
+    public List<Fornecedor> listarTodos() {
+        return fornecedorRepository.findAll();
+    }
+
+    public Optional<Fornecedor> buscarPorId(Long id) {
+        return fornecedorRepository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        fornecedorRepository.deleteById(id);
+    }
+}
