bd117cd690432cb3bdd837d768445bf5829e86b0
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-24 15:20:48 -0300

    primeiro commit

---
+++ fornecedores/src/main/java/com/exercicio/fornecedores/FornecedoresApplication.java
@@ -0,0 +1,13 @@
+package com.exercicio.fornecedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornecedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornecedoresApplication.class, args);
+	}
+
+}
